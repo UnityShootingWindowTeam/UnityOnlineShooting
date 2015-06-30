bd72a26b7bfd3d5df30d8991efeaca8b60e5002b
makegumi1514 <makegumi1514@gmail.com>
2015-06-30 21:31:04 +0900

    移動操作の変更と弾を撃てるようにした

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private NetworkView view;
+
+	void Start () {
+		view = GetComponent<NetworkView> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * 0.5f;
+		view.RPC (
+			"MoveBullet",
+			RPCMode.Others,
+			transform.position);
+	}
+
+	[RPC]
+	public void MoveBullet(Vector3 position){
+		transform.position = position;
+	}
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class Controller : MonoBehaviour {
 
 	public bool isMine;
+	public GameObject bullet;
+
+	void Start(){
+		bullet = (GameObject)Resources.Load ("Bullet");
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -14,17 +19,30 @@ public class Controller : MonoBehaviour {
 		if(isMine){
 			float dx = Input.GetAxis ("Horizontal");
 			float dy = Input.GetAxis ("Vertical");
-			transform.Translate (dx*0.5f,dy*0.5f, 0);
+			//transform.Translate (dx*0.5f,dy*0.5f, 0);
+			transform.RotateAround (transform.position, transform.up, dx*1.5f);
+			transform.position += transform.forward*dy*0.5f;
 
 			GetComponent<NetworkView>().RPC (
 				"MovePlayer",
 				RPCMode.Others,
 				transform.position);
+
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				GetComponent<NetworkView> ().RPC (
+					"Shoot",
+					RPCMode.All);
+			}
 		}
 	}
 
 	[RPC]
 	public void MovePlayer(Vector3 position){
 		transform.position = position;
+	}
+
+	[RPC]
+	public void Shoot(){
+		Instantiate (bullet, transform.position, transform.rotation);
 	}
 }
