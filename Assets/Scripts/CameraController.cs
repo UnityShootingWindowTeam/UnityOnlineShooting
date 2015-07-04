9fc09fcdd0cb2e33c810df93122426886caf521d
makegumi1514 <makegumi1514@gmail.com>
2015-07-04 14:03:46 +0900

    カメラをスクリプトで制御、床を作り色を付けた

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private GameObject floor;
+
+	// Use this for initialization
+	void Start () {
+		floor = GameObject.Find ("Floor");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = floor.transform.position + new Vector3 (0, 20, 0);
+		transform.LookAt (floor.transform);
+	}
+}
